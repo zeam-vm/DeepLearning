f3f42ccff6fb13f93fc2d554f1304a3173630034
sasagawa888 <ken-s@tkcnf.or.jp>
2019-06-22 15:15:28 +0900

    Added Test.dnn/2

--- lib/test.ex
+++ lib/test.ex
@@ -3,9 +3,9 @@ defmodule Test do
 
   # for DNN test
   defnetwork init_network1(_x) do
-    _x |> w(784,300,0.01,1) |> b(300) |> relu
-    |> w(300,100,0.01,1) |> b(100) |> relu
-    |> w(100,10,0.01,1) |> b(10) |> softmax
+    _x |> w(784,300) |> b(300) |> relu
+    |> w(300,100) |> b(100) |> relu
+    |> w(100,10) |> b(10) |> softmax
   end
 
   # for sgd test
